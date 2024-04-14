e7f35182342e0d0b0d94b1e873d7532d54a7548f
Blobby-py <thomas3sep@hotmail.com>
2024-04-14 16:39:21 +0200

    Created dummy data

--- iatbdApp/data.php
+++ iatbdApp/data.php
@@ -2,48 +2,43 @@
 
 return [
   [
-      'title' => 'Laravel Senior Developer', 
-      'tags' => 'laravel, javascript',
-      'company' => 'Acme Corp',
+      'title' => 'Product 1', 
+      'tags' => 'tag1, tag2',
+      'product' => 'product 1 name',
       'location' => 'Boston, MA',
       'email' => 'email1@email.com',
-      'website' => 'https://www.acme.com',
       'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
   ],
   [
-    'title' => 'Full-Stack Engineer',
-    'tags' => 'laravel, backend ,api',
-    'company' => 'Stark Industries',
+    'title' => 'Product 2',
+    'tags' => 'tag1, tag2, tag3',
+    'product' => 'product 2 name',
     'location' => 'New York, NY',
     'email' => 'email2@email.com',
-    'website' => 'https://www.starkindustries.com',
     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
   ],
   [
-    'title' => 'Laravel Developer', 
-    'tags' => 'laravel, vue, javascript',
-    'company' => 'Wayne Enterprises',
+    'title' => 'Product 3', 
+    'tags' => 'tag 2, tag 3, tag 4',
+    'product' => 'product 3 name',
     'location' => 'Gotham, NY',
     'email' => 'email3@email.com',
-    'website' => 'https://www.wayneenterprises.com',
     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
   ],
   [
-    'title' => 'Backend Developer', 
-    'tags' => 'laravel, php, api',
-    'company' => 'Skynet Systems',
+    'title' => 'Product 4', 
+    'tags' => 'tag1, tag2, tag3',
+    'product' => 'product 4 name',
     'location' => 'Newark, NJ',
     'email' => 'email4@email.com',
-    'website' => 'https://www.skynet.com',
     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
   ],
   [
-    'title' => 'Junior Developer', 
-    'tags' => 'laravel, php, javascript',
-    'company' => 'Wonka Industries',
+    'title' => 'Product 5', 
+    'tags' => 'tag3, tag4, tag5',
+    'product' => 'product 5 name',
     'location' => 'Boston, MA',
     'email' => 'email4@email.com',
-    'website' => 'https://www.wonka.com',
     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
   ]
 ];
