a0ceb0ce6ad9c9e79a4d8f3bb14ac505c31ba0db
Blobby-py <thomas3sep@hotmail.com>
2024-04-20 17:22:40 +0200

    Search and filter

--- iatbdApp/app/Http/Controllers/ListingController.php
+++ iatbdApp/app/Http/Controllers/ListingController.php
@@ -9,9 +9,10 @@ class ListingController extends Controller
 {
 
     // Show all listings
-    public function index() {
+    public function index(Request $request) {
+        // dd(request("tag"));
         return view('listings.index', [
-            "listings" => Listing::all()
+            "listings" => Listing::latest()->filter(request(["tag", "search"]))->get()
         ]);
     }
 
